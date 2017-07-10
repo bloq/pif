7cd67a9845cffbac6a96f61db8be4dbac93b074c
Jeff Garzik <jeff@bloq.com>
2017-07-10 03:55:57 -0400

    [test] Import zeppelin scripts and helpers.  Begin to verify time lock.

--- test/paytofutureminer.js
+++ test/paytofutureminer.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const moment = require('moment');
+
+// Helper: create and send a transaction, transferring some value
 let sendTx = function(sender, receiver, value) {
   web3.eth.sendTransaction({
     from:	sender,
@@ -7,6 +10,35 @@ let sendTx = function(sender, receiver, value) {
     value:	value
   });
 };
+
+// Helper: shorthand for $x ETH
+function ether(n) {
+  return web3.toWei(n, 'ether');
+}
+
+// Helper: Increases testrpc time by the passed duration (a moment.js instance)
+function increaseTime(duration) {
+  const id = Date.now()
+
+  return new Promise((resolve, reject) => {
+    web3.currentProvider.sendAsync({
+      jsonrpc: '2.0',
+      method: 'evm_increaseTime',
+      params: [duration.asSeconds()],
+      id: id,
+    }, err1 => {
+      if (err1) return reject(err1)
+
+      web3.currentProvider.sendAsync({
+        jsonrpc: '2.0',
+        method: 'evm_mine',
+        id: id+1,
+      }, (err2, res) => {
+        return err2 ? reject(err2) : resolve(res)
+      })
+    })
+  })
+}
 
 var PayToFutureMiner = artifacts.require("./PayToFutureMiner.sol");
 
@@ -33,13 +65,13 @@ contract('PayToFutureMiner', function(accounts) {
     let contract = await PayToFutureMiner.deployed();
 
     // Give account[0] 20 ether
-    sendTx(web3.eth.coinbase, accounts[0], web3.toWei('20','ether'));
+    sendTx(web3.eth.coinbase, accounts[0], ether(20));
 
     // Send 3 ether to contract
-    sendTx(accounts[0], contract.address, web3.toWei('3','ether'));
+    sendTx(accounts[0], contract.address, ether(3));
 
     // Verify contract balance after send
-    let expected = web3.toWei('3','ether');
+    let expected = ether(3);
     assert.equal(expected, web3.eth.getBalance(contract.address).toNumber());
 
     // Verify just-sent ether is frozen
@@ -52,6 +84,38 @@ contract('PayToFutureMiner', function(accounts) {
 
     expected = await contract.balanceDrawable();
     assert.equal(expected, 0, "balanceDrawable must be zero");
+  });
+
+  it('verify time lock expires', async function() {
+    let contract = await PayToFutureMiner.deployed();
+
+    // Verify total contract balance
+    let expected = ether(3);
+    assert.equal(expected, web3.eth.getBalance(contract.address).toNumber());
+
+    // Increase time to expire-1
+    await increaseTime(moment.duration(contract.frozenPeriod - 1, 'day'));
+
+    // Verify total contract balance again
+    expected = ether(3);
+    assert.equal(expected, web3.eth.getBalance(contract.address).toNumber());
+
+    // Verify sent ether is still frozen
+    let balance = await contract.balanceFrozen();
+    assert.equal(expected, balance);
+
+    // Verify other balances still zero
+    expected = await contract.balanceThawed();
+    assert.equal(expected, 0, "balanceThawed must be zero");
+
+    expected = await contract.balanceDrawable();
+    assert.equal(expected, 0, "balanceDrawable must be zero");
+
+    // Increase time to expire
+    await increaseTime(moment.duration(1, 'day'));
+
+    // TODO: actually verify time lock expired
+
   });
 
 });
